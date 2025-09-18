894539ce891f929d9c1ab6bcd0db5d4f2edaaa39
Lhomme Aymeric <aymericlhomme@orange.fr>
2025-09-18 15:24:05 +0200

    feat(view)
    - Simplification of inputs

--- src/resources/views/admin/tenants/form.blade.php
+++ src/resources/views/admin/tenants/form.blade.php
@@ -28,13 +28,13 @@
                 @csrf
                 @method($tenant->exists ? 'put' : 'post')
                 <div class="grid">
-                    @include('core-cms::shared.input', ['label' => __('multi-tenancy::admin.tenant.id'), 'name' => 'id', 'value' => $tenant->id, 'disabled' => $tenant->exists])
-                    @include('core-cms::shared.input', ['label' => __('multi-tenancy::admin.tenant.name'), 'name' => 'name', 'value' => $tenant->name])
-                    @include('core-cms::shared.input', ['label' => __('multi-tenancy::admin.tenant.owner.name'), 'name' => 'owner[name]', 'value' => $tenant->owner['name'] ?? ''])
-                    @include('core-cms::shared.input', ['label' => __('multi-tenancy::admin.tenant.owner.email'), 'name' => 'owner[email]', 'value' => $tenant->owner['email'] ?? ''])
-                    @include('core-cms::shared.input', ['label' => __('multi-tenancy::admin.tenant.owner.status'), 'name' => 'owner[status]', 'value' => $tenant->owner['status'] ?? ''])
-                    @include('core-cms::shared.input', ['label' => __('multi-tenancy::admin.tenant.owner.address'), 'name' => 'owner[address]', 'value' => $tenant->owner['address'] ?? ''])
-                    @include('core-cms::shared.input', ['label' => __('multi-tenancy::admin.tenant.owner.siret'), 'name' => 'owner[siret]', 'value' => $tenant->owner['siret'] ?? ''])
+                    @include('core-cms::shared.form-field', ['label' => __('multi-tenancy::admin.tenant.id'), 'name' => 'id', 'value' => $tenant->id, 'disabled' => $tenant->exists])
+                    @include('core-cms::shared.form-field', ['label' => __('multi-tenancy::admin.tenant.name'), 'name' => 'name', 'value' => $tenant->name])
+                    @include('core-cms::shared.form-field', ['label' => __('multi-tenancy::admin.tenant.owner.name'), 'name' => 'owner[name]', 'value' => $tenant->owner['name'] ?? ''])
+                    @include('core-cms::shared.form-field', ['label' => __('multi-tenancy::admin.tenant.owner.email'), 'name' => 'owner[email]', 'value' => $tenant->owner['email'] ?? ''])
+                    @include('core-cms::shared.form-field', ['label' => __('multi-tenancy::admin.tenant.owner.status'), 'name' => 'owner[status]', 'value' => $tenant->owner['status'] ?? ''])
+                    @include('core-cms::shared.form-field', ['label' => __('multi-tenancy::admin.tenant.owner.address'), 'name' => 'owner[address]', 'value' => $tenant->owner['address'] ?? ''])
+                    @include('core-cms::shared.form-field', ['label' => __('multi-tenancy::admin.tenant.owner.siret'), 'name' => 'owner[siret]', 'value' => $tenant->owner['siret'] ?? ''])
                     <div class="text-center">
                         <button type="submit" class="button" data-type="primary">{{ __('core-cms::admin.save') }}</button>
                     </div>
